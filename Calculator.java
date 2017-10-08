470a9bf6353fd1e45442cbdd96a2d5a5b703a748
Anastasiya Sudarenko <snastiya@mail.ru>
2017-10-08 19:41:59 +0300

    1.0

--- Calculator.java
+++ Calculator.java
@@ -9,6 +9,8 @@ public class Calculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+//Ввод переменных
         System.out.println("Введите первое число: ");
         float first = scanner.nextFloat();
         System.out.println("Введите второе число: ");
